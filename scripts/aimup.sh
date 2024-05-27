5f444ccf00ae90a195e3c4807e480c1acc29d66a
jczhang02 <jczhang@live.it>
2024-05-27 15:47:43 +0800

    chore(scripts): modify aimup script

--- scripts/aimup.sh
+++ scripts/aimup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --job-name=Aimup
 #SBATCH --partition=LocalQ
-#SBATCH --output=/zhangchrai23/OperatorFormer/logs/sbatch/aimup/aimup.out
-#SBATCH --error=/zhangchrai23/OperatorFormer/logs/sbatch/aimup/aimup_error.out
-aim up --host 0.0.0.0 -p 6006
+#SBATCH --output=/zhangchrai23/logs/aimup.out
+aim up --host 0.0.0.0 -p 6007
