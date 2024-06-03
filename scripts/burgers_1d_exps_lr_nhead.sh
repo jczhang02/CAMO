ce4e5c7a132625a71c5620c71ca592a8bbf89ea5
jczhang02 <jczhang@live.it>
2024-06-03 11:15:19 +0800

    chore(exp): modify exps

--- scripts/burgers_1d_exps_lr_nhead.sh
+++ scripts/burgers_1d_exps_lr_nhead.sh
@@ -7,7 +7,8 @@
 
 python src/train.py \
 	experiment=burgers \
-	model.net.input_encoder_config.nhead=1,4 \
-	model.optimizer.lr=1.0e-4,3.5e-4,8.0e-4 \
-	task_name="Burgers_1D_exps_lr_nhead" \
+	model.net.input_encoder_config.nhead=4 \
+	model.optimizer.lr=1.0e-3,1.0e-4,3.5e-4,8.0e-4 \
+	trainer.deterministic=True \
+	task_name="Burgers_1D_exps_lr" \
 	-m
---
+++ scripts/burgers_optuna.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=optuna_Burgers_1D
+#SBATCH --partition=local
+#SBATCH --output=/zhangchrai23/logs/optuna_Burgers_1D.out
+
+# hyperparameter optimization using Optuna
+python src/train.py \
+	experiment=burgers_optuna \
+	trainer.deterministic=True \
+	hparams_search=burgers_optuna \
+	task_name="Optuna_Burgers" \
+	-m
